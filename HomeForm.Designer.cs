d84a14d6136b20655be437728bb3fd9ab9e30992
ElSabadello <mattia.sabadell@gmail.com>
2025-06-29 16:14:58 +0200

    New InsertForm Created and Linked to the HomeForm

---
+++ HomeForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace projectStudentManagement
+{
+    partial class HomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            Add_bt = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(205, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(583, 426);
+            dataGridView1.TabIndex = 0;
+            // 
+            // Add_bt
+            // 
+            Add_bt.Font = new Font("Segoe UI", 13F);
+            Add_bt.Location = new Point(12, 12);
+            Add_bt.Name = "Add_bt";
+            Add_bt.Size = new Size(121, 56);
+            Add_bt.TabIndex = 1;
+            Add_bt.Text = "Add";
+            Add_bt.UseVisualStyleBackColor = true;
+            Add_bt.Click += Add_bt_Click;
+            // 
+            // HomeForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(Add_bt);
+            Controls.Add(dataGridView1);
+            Name = "HomeForm";
+            Text = "Student Management - HOME";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button Add_bt;
+    }
+}
---
+++ InsertForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace projectStudentManagement
+{
+    partial class InsertForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            RegNo_tx = new TextBox();
+            Save_bt = new Button();
+            Name_tx = new TextBox();
+            label2 = new Label();
+            Aaddress_tx = new TextBox();
+            label3 = new Label();
+            Cancel_bt = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Reg No.";
+            // 
+            // RegNo_tx
+            // 
+            RegNo_tx.Location = new Point(12, 32);
+            RegNo_tx.Name = "RegNo_tx";
+            RegNo_tx.Size = new Size(179, 27);
+            RegNo_tx.TabIndex = 1;
+            // 
+            // Save_bt
+            // 
+            Save_bt.Location = new Point(367, 77);
+            Save_bt.Name = "Save_bt";
+            Save_bt.Size = new Size(94, 29);
+            Save_bt.TabIndex = 2;
+            Save_bt.Text = "Save";
+            Save_bt.UseVisualStyleBackColor = true;
+            // 
+            // Name_tx
+            // 
+            Name_tx.Location = new Point(197, 32);
+            Name_tx.Name = "Name_tx";
+            Name_tx.Size = new Size(179, 27);
+            Name_tx.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(197, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(49, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Name";
+            // 
+            // Aaddress_tx
+            // 
+            Aaddress_tx.Location = new Point(382, 32);
+            Aaddress_tx.Name = "Aaddress_tx";
+            Aaddress_tx.Size = new Size(179, 27);
+            Aaddress_tx.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(382, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(62, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Address";
+            // 
+            // Cancel_bt
+            // 
+            Cancel_bt.Location = new Point(467, 77);
+            Cancel_bt.Name = "Cancel_bt";
+            Cancel_bt.Size = new Size(94, 29);
+            Cancel_bt.TabIndex = 7;
+            Cancel_bt.Text = "Cancel";
+            Cancel_bt.UseVisualStyleBackColor = true;
+            // 
+            // InsertForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(575, 126);
+            Controls.Add(Cancel_bt);
+            Controls.Add(Aaddress_tx);
+            Controls.Add(label3);
+            Controls.Add(Name_tx);
+            Controls.Add(label2);
+            Controls.Add(Save_bt);
+            Controls.Add(RegNo_tx);
+            Controls.Add(label1);
+            Name = "InsertForm";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox RegNo_tx;
+        private Button Save_bt;
+        private TextBox Name_tx;
+        private Label label2;
+        private TextBox Aaddress_tx;
+        private Label label3;
+        private Button Cancel_bt;
+    }
+}
